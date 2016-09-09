4b5f03dca54dae9dbaa2bb8cbc93c36007ffe9e4
Nick James <nick.james@eaglegenomics.com>
2016-09-09 23:14:24 +0100

    removed poise-python version restriction as conflicts with ehive-docker CB

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures metaphlan2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/metaphlan2'
 issues_url 'https://github.com/EagleGenomics-cookbooks/metaphlan2/issues'
 
 depends 'apt'
 depends 'magic_shell'
-depends 'poise-python', '~> 1.4.0'
+depends 'poise-python'
 depends 'mercurial', '~> 2.0.4'
